9c14261c2b595c200a96d60f717e7dbe7a477079
Bruno <bruno.buck1@hotmail.com>
2021-10-27 15:21:57 -0300

    update

--- src/Components/SectionOne/index.js
+++ src/Components/SectionOne/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Logo from '../../assets/background.svg';
 
-import { Container } from './styles';
+import { Container, Box, Title, Button } from './styles';
 
 function SectionOne() {
     return (
         <Container>
             <img src={Logo} alt="/"/>
-            
+            <Box>
+                <Title>Explore the life you want to live. Put your money to work</Title>
+                <Button>Get it touch</Button>
+            </Box>
         </Container>
     );
 }
--- src/Components/SectionOne/styles.js
+++ src/Components/SectionOne/styles.js
@@ -6,23 +6,51 @@ export const Container = styled.div`
     margin-top: 50px;
     position: relative;
 
-    background-image: url('https://www.10wallpaper.com/wallpaper/1366x768/1201/purple_nebula-Space_Photography_Desktop_Wallpaper_1366x768.jpg');
-    background-position: center;
-    background-size: cover;
-    background-repeat: no-repeat;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background: url('https://www.10wallpaper.com/wallpaper/1366x768/1201/purple_nebula-Space_Photography_Desktop_Wallpaper_1366x768.jpg') no-repeat center center/cover;
 
     /* Overlay */
     background-color: rgba(0,0,0,0.6);
     background-blend-mode: color;
     
-    
-   /*  border: solid 1px #000;
-    border-radius: 0 0 240px 50%/60px;
-    background-color: yellow; */
-
     img{
         position: absolute;
         bottom: 0px;
     }
     
+`;
+
+
+export const Box = styled.div`
+    width: 400px;    
+    position: absolute;
+
+`;
+export const Title = styled.h1`
+    font-size: 1.5rem;
+    color: #fff;
+    font-weight:bold;
+    letter-spacing: 1px;
+    margin-bottom: 20px;
+    line-height: 2rem;
+
+`;
+export const Button = styled.button`
+    width: 180px;
+    padding: 12px 15px;
+    background-color: #4f3d74;
+    color: #fff;
+
+    font-size: 1rem;
+    border-radius: 20px;
+    cursor: pointer;
+    transition: ease 0.2s;
+
+    &:hover{
+        background-color: rgba(0,0,0,0.6);
+    }
+
 `;
