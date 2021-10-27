a5f095345828146a20838281893661bb68f59768
Bruno <bruno.buck1@hotmail.com>
2021-10-27 09:14:03 -0300

    create styles and dark mode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import React, {useState} from 'react';
+
+import { ThemeProvider } from 'styled-components'
+import {Dark, Light} from './Styles/Themes'
+import GlobalStyles from './Styles/GlobalStyles'
+
+
+
 
 function App() {
+
+  const [theme, setTheme] = useState(Dark)
+  const togleTheme = () =>{
+    setTheme(theme.title === 'dark' ? Light : Dark)
+  }
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <div >
+        <h1>Hello World</h1>
+        <input type="checkbox" onChange={togleTheme} />
+      </div>
+      <GlobalStyles/>
+    </ThemeProvider>
+  )
+
 }
 
 export default App;
---
+++ src/Styles/Themes.js
@@ -0,0 +1,19 @@
+export const Light = {
+    title: 'light',
+
+    colors: {
+        primary: '#ccc',
+        background: '#fff',
+        text: '#111'
+    }
+}
+
+export const Dark = {
+    title: 'dark',
+
+    colors: {
+        primary: '#ccf',
+        background: '#111',
+        text: '#fff'
+    }
+}
